39507464dbf2348d874b3352260e964a4c0f5368
SITC-QBcourse <richyyang1314@shirahagi.ac.jp>
2025-02-05 17:23:57 +0900

    老杨
    恶意修改

--- src/com/koneko/consulting/com/Demo01.java
+++ src/com/koneko/consulting/com/Demo01.java
@@ -3,5 +3,6 @@ package com.koneko.consulting.com;
 public class Demo01 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello world!");
 	}
 }
